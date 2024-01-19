d82be699addf478dbb17c1a772d9d270fe752088
KwameGH22 <adarkwa.y.k@gmail.com>
2024-01-19 22:26:14 +0000

    taskview component reviewed

--- src/components/TaskView.jsx
+++ src/components/TaskView.jsx
@@ -13,8 +13,8 @@ const TaskView = () => {
     <div className={`${darkTheme ? 'bg-[#0e033d]  text-white' : 'bg-white' } flex  p-4 h-12 items-center justify-between text-blue-500 lg:w-[50%] md:w-[50%] m-auto shadow-2xl mt-1 mb-1 max-sm:mx-[5%] `}>
       
       <div className="items-center flex p-7 justify-between gap-5  ">
-        {todos.complete ? <MdCheck className="h-5 w-5 text-blue-500" onClick={() => handleToggle(todo.id)} /> :<MdCancel className="h-5 w-5 text-blue-500" onClick={() => handleToggle(todos.id)} /> }
-      <h2 className={`${todos.complete && "line-through"} `}>{todos.title}</h2>
+        {todos.complete ? <MdCheck className="h-5 w-5 text-blue-500" onClick={() => handleToggle(todos.id)} /> :<MdCancel className="h-5 w-5 text-blue-500" onClick={() => handleToggle(todos.id)} /> }
+        <h2 className={`${todos.complete && "line-through"} `}>{todos.title}</h2>
       </div>
       <div>
        <MdDeleteForever className="col text-red-500  h-5 w-5" onClick={()=> removeTodo(todos.id)}/>
