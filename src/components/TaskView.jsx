dbfb793c692f9f9d31efad9a267b053c1c0ef6ab
KwameGH22 <adarkwa.y.k@gmail.com>
2024-02-07 16:50:04 +0000

    todo.title now appearing

--- src/components/TaskView.jsx
+++ src/components/TaskView.jsx
@@ -1,27 +1,40 @@
-import React, { useContext } from 'react'
-import { AppContext } from '../context/AppContextProvider'
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContextProvider";
 import { MdCancel, MdDeleteForever, MdCheck } from "react-icons/md";
 
-const TaskView = () => {
-    const {darkTheme, handleToggle, todos, removeTodo } = useContext(AppContext)
-
-
-
-
+const TaskView = (todo) => {
+  const { darkTheme, handleToggle, todos, removeTodo } = useContext(AppContext);
+  
+  console.log(todo);
 
   return (
-    <div className={`${darkTheme ? 'bg-[#0e033d]  text-white' : 'bg-white' } flex  p-4 h-12 items-center justify-between text-blue-500 lg:w-[50%] md:w-[50%] m-auto shadow-2xl mt-1 mb-1 max-sm:mx-[5%] `}>
-      
+    <div
+      className={`${
+        darkTheme ? "bg-[#0e033d]  text-white" : "bg-white"
+      } flex  p-4 h-12 items-center justify-between text-blue-500 lg:w-[50%] md:w-[50%] m-auto shadow-2xl mt-1 mb-1 max-sm:mx-[5%] `}
+    >
       <div className="items-center flex p-7 justify-between gap-5  ">
-        {todos.complete ? <MdCheck className="h-5 w-5 text-blue-500" onClick={() => handleToggle(todos.id)} /> : <MdCancel className="h-5 w-5 text-blue-500" onClick={() => handleToggle(todos.id)} /> }
-        <h2 className={`${todos.complete && "line-through"} `}>{todos.title}</h2>
+        {todo?.complete ? (
+          <MdCheck
+            className="h-5 w-5 text-blue-500"
+            onClick={() => handleToggle(todo.id)}
+          />
+        ) : (
+          <MdCancel
+            className="h-5 w-5 text-blue-500"
+            onClick={() => handleToggle(todo.id)}
+          />
+        )}
+        <h2 className={`${todo?.complete && "line-through"} `}>{todo?.title}</h2>
       </div>
       <div>
-       <MdDeleteForever className="col text-red-500  h-5 w-5" onClick={()=> removeTodo(todos.id)}/>
+        <MdDeleteForever
+          className="col text-red-500  h-5 w-5"
+          onClick={() => removeTodo(todo?.id)}
+        />
       </div>
-    
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default TaskView
+export default TaskView;
--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -1,118 +1,111 @@
-import React, { createContext, useState } from 'react';
-import TaskView from '../components/TaskView';
-
+import React, { createContext, useState } from "react";
+import TaskView from "../components/TaskView";
 
 export const AppContext = createContext();
 
-const AppContextProvider = ({children}) => {
-    const [todos, setTodos] = useState([]);
-    const [itemList, setItemList] = useState("");
-    const [filter, setFilter] = useState("all");
-    const [darkTheme, setDarkTheme] = useState(false);
+const AppContextProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+  const [itemList, setItemList] = useState("");
+  const [filter, setFilter] = useState("all");
+  const [darkTheme, setDarkTheme] = useState(false);
 
+  const addItem = (text) => {
+    const newList = {
+      id: Date.now(),
+      title: text,
+      complete: false,
+    };
+    setTodos([...todos, newList]);
+  };
+  console.log(todos);
 
-    const addItem = (text) => {
-        const newList = {
-          id: Date.now(),
-          title: text,
-          complete: false,
+  const handleToggle = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete,
+          };
         }
-        setTodos([newList, ...todos])
-      }
+        return todo;
+      })
+    );
+  };
 
-    const handleToggle = (id) => {
-        setTodos(
-          todos.map(todo => {
-            if (todo.id === id){
-              return {
-                ...todo,
-                complete: !todo.complete
-              }
-            }   
-              return todo
-            
-          })
-        )
-      }
+  const removeTodo = (id) => {
+    const newList = todos.filter((todo) => todo.id !== id);
+    setTodos(newList);
+  };
 
-    const removeTodo = (id) => {
-        const newList = todos.filter((todo) => todo.id !== id)
-        setTodos(newList)
-      }
-    
-    const setFilterValue = (value) => setFilter(value);
+  const setFilterValue = (value) => setFilter(value);
 
-    const keepFiltering = () => {
-        if (filter === "all") {
-          return todos.map(todo => (
-            <TaskView key={todo.id} {...todo}/>
-          ));
-        } else if (filter === "active") {
-          return todos.filter(todo => !todo.complete).map(todo => (
-            <TaskView key={todo.id} {...todo}/>
-          ));
-        } else if (filter === "complete") {
-          return todos.filter(todo => todo.complete).map(todo => (
-            <TaskView key={todo.id} {...todo}/>
-          ));
-        }
-        return null; // Nothing to return
-      };
+  const keepFiltering = () => {
+    if (filter === "all") {
+      return todos.map((todo) => <TaskView key={todo.id} {...todo} />);
+    } else if (filter === "active") {
+      return todos
+        .filter((todo) => !todo.complete)
+        .map((todo) => <TaskView key={todo.id} {...todo} />);
+    } else if (filter === "complete") {
+      return todos
+        .filter((todo) => todo.complete)
+        .map((todo) => <TaskView key={todo.id} {...todo} />);
+    }
+    return null; // Nothing to return
+  };
 
-    const completedItems = () => {
-        const clearCompletedItems = todos.filter(
-          todo => !todo.complete
-        )
-        setTodos(clearCompletedItems)
-      }
-    
-    const getCount = () => {
-        if (filter === "all") {
-          return todos.length
-        } else if (filter === "active"){
-          return todos.filter(
-            todo => !todo.complete
-          ).length
-        } else if (filter === "complete"){
-          return todos.filter(
-            todo => todo.complete
-          ).length
-        }
-        return 0
-      }
+  const completedItems = () => {
+    const clearCompletedItems = todos.filter((todo) => !todo.complete);
+    setTodos(clearCompletedItems);
+  };
 
-    const handleTheming = () => {
-        setDarkTheme(!darkTheme);
-      }
-  
-    const handleSubmit = (event) => {
-        event.preventDefault();    
-        addItem(itemList)
-        setItemList("");
-      }
-    
+  const getCount = () => {
+    if (filter === "all") {
+      return todos.length;
+    } else if (filter === "active") {
+      return todos.filter((todo) => !todo.complete).length;
+    } else if (filter === "complete") {
+      return todos.filter((todo) => todo.complete).length;
+    }
+    return 0;
+  };
 
+  const handleTheming = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addItem(itemList);
+    setItemList("");
+  };
 
   return (
-    <AppContext.Provider value={{todos, setTodos, 
-                                itemList, setItemList,
-                                filter, setFilter,
-                                darkTheme, setDarkTheme,
-                                addItem, handleToggle,
-                                removeTodo, setFilterValue,
-                                keepFiltering, completedItems,
-                                getCount, handleTheming,
-                                handleSubmit
-
-
-
-
-    }}>
-        {children}
+    <AppContext.Provider
+      value={{
+        todos,
+        setTodos,
+        itemList,
+        setItemList,
+        filter,
+        setFilter,
+        darkTheme,
+        setDarkTheme,
+        addItem,
+        handleToggle,
+        removeTodo,
+        setFilterValue,
+        keepFiltering,
+        completedItems,
+        getCount,
+        handleTheming,
+        handleSubmit,
+      }}
+    >
+      {children}
     </AppContext.Provider>
-      
-    
-  )
-}
+  );
+};
 
-export default AppContextProvider
+export default AppContextProvider;
